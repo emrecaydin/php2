51c1d20f2b087723cd04031185721959e0d64c01
emrecaydin <70521265+emrecaydin@users.noreply.github.com>
2020-09-28 09:46:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$movieName = "Indiana Jones and the Last Crusade"
+$seenBefore= false
+$movieNote= 8.2
+$movieDate= "18 October 1989"
+
+echo $movieName
+echo seenBefore
+echo $movieNote
+echo $movieDate
+?>
